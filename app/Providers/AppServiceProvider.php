38b5b53099fff518a2638b716f6f2cddf478422e
chinhnv <nguyenchinh97tx@gmail.com>
2020-03-17 09:53:54 +0700

    add css

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    use URL;
+
 //
     public function boot()
     {
